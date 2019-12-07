7f715d76ef8f7e8580d46198d0b6e1a0ff7754ad
angel2282 <vbalazs821125@gmail.com>
2019-12-07 15:40:10 +0100

    first commit

---
+++ newcode/code.php
@@ -0,0 +1,21 @@
+<?php
+echo("codelist!");
+function clean($string) {
+   $string = str_replace(' ', '-', $string);
+   $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string);
+   $string = str_replace('--', '-', $string);
+   return $string; }
+$json = file_get_contents("http://192.168.1.149:9876/codes");
+$json_de = json_decode($json,true);
+
+
+foreach ($json_de as $data) {
+ print $data["code"]." name:".clean($data["displayName"])."<br>";
+ $myfile = fopen(clean($data["displayName"]), "x+");
+ $txt = $data["code"];
+ fwrite($myfile, $txt);
+ fclose($myfile);
+}
+
+
+?>
